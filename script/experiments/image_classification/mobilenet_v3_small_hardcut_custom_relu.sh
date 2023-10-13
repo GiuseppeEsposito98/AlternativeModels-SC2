125639dad860c7db1719fd2ea03198d1f4c24423
giuseppeesposito98 <giuseppeesposito98@libero.it>
2023-10-13 10:59:28 +0200

    add:changing tree and run bash files for sc2 train

---
+++ script/experiments/image_classification/mobilenet_v3_small_hardcut_custom_relu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source ~/miniconda3/bin/activate
+conda deactivate
+
+cd  /home/gesposito/AlternativeModels\-SC2
+
+conda activate sc2-benchmark
+
+
+python script/task/image_classification.py \
+        --config configs/ilsvrc2012/supervised_compression/ghnd-bq/MobileNetV3Small-bq6ch_from_MobileNetV3Small_hardcut_custom_relu.yaml \
+        --device cpu\
+        --log script/task/logs/mobilenet_v3_small_hardcut_custom_relu.log \
+        --writerlog /home/gesposito/AlternativeModels\-SC2/script/task/writers_logs_mobilenet_v3_small_hardcut_custom_relu \
--- script/experiments/object_detection/ssd300_vgg16_run_layer_swap.sh
+++ script/experiments/object_detection/ssd300_vgg16_run_layer_swap.sh
@@ -3,7 +3,7 @@
 source ~/miniconda3/bin/activate
 conda deactivate
 
-cd  /home/g.esposito/AlternativeModels\-SC2
+cd  /home/gesposito/AlternativeModels\-SC2
 
 conda activate sc2-benchmark
 
@@ -12,4 +12,4 @@ python script/task/object_detection_ssd.py \
         --config configs/coco2017/supervised_compression/ghnd-bq/ssd300_vgg16-bq1ch_from_ssd300_vgg16_hardcut_layer_swap.yaml \
         --device cuda\
         --log script/task/logs/log_layer_swap.log \
-        --writerlog /home/g.esposito/AlternativeModels\-SC2/script/task/writers_logs_layer_swap \
+        --writerlog script/experiment/writers_logs_layer_swap \
