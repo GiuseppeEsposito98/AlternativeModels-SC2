23df4871c3140f3ca23d4e186424fab88f663055
giuseppeesposito98 <giuseppeesposito98@libero.it>
2024-01-19 23:56:17 +0100

    add: ranger training and keep forward hook

--- script/experiments/image_classification/cifar/keep_relu_values.sh
+++ script/experiments/image_classification/cifar/keep_relu_values.sh
@@ -8,7 +8,7 @@ cd  /home/gesposito/AlternativeModels\-SC2
 conda activate sc2-benchmark
 
 
-python script/task/image_classification_to_delete.py \
+python script/task/image_classification_inter_output_hook.py \
         --config configs/cifar100/MobileNetV3Small-bq6ch_from_MobileNetV3Small_cov13.yaml  \
         --device cuda\
         --log script/task/logs/mobilenet_v3_small_hardcut_keep_relu.log \
---
+++ script/ranger_tuning/ranger_img_classification_FI.py
@@ -0,0 +1,312 @@
+import argparse
+import datetime
+import json
+import os
+import time
+
+import torch
+from copy import deepcopy
+from torch import distributed as dist
+from torch.backends import cudnn
+from torch.nn import DataParallel
+from torch.nn.parallel import DistributedDataParallel
+from torch.utils.data import RandomSampler
+from torchdistill.common import file_util, yaml_util, module_util
+from torchdistill.common.constant import def_logger
+from torchdistill.common.main_util import is_main_process, init_distributed_mode, load_ckpt, save_ckpt, set_seed
+from torchdistill.core.distillation import get_distillation_box
+from torchdistill.core.training import get_training_box
+from torchdistill.datasets import util
+from torchdistill.eval.classification import compute_accuracy
+from torchdistill.misc.log import setup_log_file, SmoothedValue, MetricLogger
+
+from sc2bench.analysis import check_if_analyzable
+from sc2bench.common.config_util import overwrite_config
+from sc2bench.models.backbone import check_if_updatable
+from sc2bench.models.registry import load_classification_model
+from sc2bench.models.wrapper import get_wrapped_classification_model
+
+from pytorchfi.FI_Weights_classification import FI_report_classifier
+from pytorchfi.FI_Weights_classification import FI_framework
+from pytorchfi.FI_Weights_classification import FI_manager 
+from pytorchfi.FI_Weights_classification import DatasetSampling 
+
+from torch.utils.data import DataLoader, Subset
+
+logger = def_logger.getChild(__name__)
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description='Supervised compression for image classification tasks')
+    parser.add_argument('--config', required=True, help='yaml file path')
+    parser.add_argument('--json', help='json string to overwrite config')
+    parser.add_argument('--device', default='cuda', help='device')
+    parser.add_argument('--log', help='log file path')
+    parser.add_argument('--seed', type=int, help='seed in random number generator')
+    parser.add_argument('-test_only', action='store_true', help='only test the models')
+    parser.add_argument('-no_dp_eval', action='store_true',
+                        help='perform evaluation without DistributedDataParallel/DataParallel')
+    parser.add_argument('-log_config', action='store_true', help='log config')
+    # distributed training parameters
+    parser.add_argument('--world_size', default=1, type=int, help='number of distributed processes')
+    parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+    parser.add_argument('-adjust_lr', action='store_true',
+                        help='multiply learning rate by number of distributed processes (world_size)')
+    return parser
+
+
+def load_model(model_config, device, distributed):
+    if 'classification_model' not in model_config:
+        return load_classification_model(model_config, device, distributed)
+    return get_wrapped_classification_model(model_config, device, distributed)
+
+def fix_model(student_model, bounding_ranges):
+    state_dict = student_model.state_dict()
+    print(student_model)
+    ranger_student = ModifiedStudent(student_model, bounding_ranges)
+    # print(ranger_student)
+    # print(state_dict.keys())
+    # print(type(state_dict))
+    for key in list(state_dict.keys()):
+        if key.startswith('features.bottleneck_layer.encoder'):
+            fields = key.split('.')
+            if fields[3] == '4':
+                k_new = 'features.bottleneck_layer.encoder.6.'+ fields[4]
+                state_dict[k_new] = state_dict.pop(key)
+            if fields[3] == '3':
+                k_new = 'features.bottleneck_layer.encoder.5.'+ fields[4]
+                state_dict[k_new] = state_dict.pop(key)
+    # print(state_dict['features.bottleneck_layer.encoder'].keys())
+    ranger_student.weak_model.load_state_dict(state_dict)
+    return ranger_student
+    
+
+class ModifiedStudent(torch.nn.Module):
+    def __init__(self, pretrained_model, bounding_ranges):
+        super(ModifiedStudent, self).__init__()
+        self.bounding_act = bounding_ranges
+
+        # Clone the pretrained model
+        self.weak_model = deepcopy(pretrained_model)
+
+        # Modify the convolutional block
+        self.modify_conv_block()
+
+    def modify_conv_block(self):
+        act_counter = 0
+        current_max = self.bounding_act[act_counter]
+        tmp = list(self.weak_model.features.bottleneck_layer.encoder.children())
+        void = list()
+        for el in tmp:
+            void.append(el)
+            if not isinstance(el, torch.nn.Conv2d) and not isinstance(el, torch.nn.Hardtanh):
+                void.append(torch.nn.Hardtanh(min_val=0, max_val=current_max))
+            elif isinstance(el, torch.nn.ReLU):
+                act_counter+=1
+        tmp_seq = torch.nn.Sequential(*void)
+        self.weak_model.features.bottleneck_layer.encoder = tmp_seq
+
+            
+    
+
+
+@torch.inference_mode()
+def evaluate(model_wo_ddp, data_loader, device, device_ids, distributed, no_dp_eval=False,
+             log_freq=1000, title=None, header='Test:', writer=None):
+    model = model_wo_ddp.to(device)
+    if distributed and not no_dp_eval:
+        model = DistributedDataParallel(model_wo_ddp, device_ids=device_ids)
+    elif device.type.startswith('cuda') and not no_dp_eval:
+        model = DataParallel(model_wo_ddp, device_ids=device_ids)
+    elif hasattr(model, 'use_cpu4compression'):
+        model.use_cpu4compression()
+
+    if title is not None:
+        logger.info(title)
+
+    model.eval()
+    analyzable = check_if_analyzable(model_wo_ddp)
+    metric_logger = MetricLogger(delimiter='  ')
+    for image, target in metric_logger.log_every(data_loader, log_freq, header):
+        if isinstance(image, torch.Tensor):
+            image = image.to(device, non_blocking=True)
+
+        if isinstance(target, torch.Tensor):
+            target = target.to(device, non_blocking=True)
+
+        output = model(image)
+        acc1, acc5 = compute_accuracy(output, target, topk=(1, 5))
+        # FIXME need to take into account that the datasets
+        # could have been padded in distributed setup
+        batch_size = len(image)
+        metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+        metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    top1_accuracy = metric_logger.acc1.global_avg
+    top5_accuracy = metric_logger.acc5.global_avg
+    logger.info(' * Acc@1 {:.4f}\tAcc@5 {:.4f}\n'.format(top1_accuracy, top5_accuracy))
+    if analyzable and model_wo_ddp.activated_analysis:
+        model_wo_ddp.summarize()
+    return metric_logger.acc1.global_avg
+
+def train_one_epoch(training_box, aux_module, bottleneck_updated, device, epoch, log_freq):
+    metric_logger = MetricLogger(delimiter='  ')
+    metric_logger.add_meter('lr', SmoothedValue(window_size=1, fmt='{value}'))
+    metric_logger.add_meter('img/s', SmoothedValue(window_size=10, fmt='{value}'))
+    uses_aux_loss = aux_module is not None and not bottleneck_updated
+    header = 'Epoch: [{}]'.format(epoch)
+    for sample_batch, targets, supp_dict in \
+            metric_logger.log_every(training_box.train_data_loader, log_freq, header):
+        if isinstance(sample_batch, torch.Tensor):
+            sample_batch = sample_batch.to(device)
+
+        if isinstance(targets, torch.Tensor):
+            targets = targets.to(device)
+
+        start_time = time.time()
+        loss = training_box(sample_batch, targets, supp_dict)
+        aux_loss = None
+        if uses_aux_loss:
+            aux_loss = aux_module.aux_loss()
+            aux_loss.backward()
+
+        training_box.update_params(loss)
+        batch_size = len(sample_batch)
+        if uses_aux_loss:
+            metric_logger.update(loss=loss.item(), aux_loss=aux_loss.item(),
+                                 lr=training_box.optimizer.param_groups[0]['lr'])
+        else:
+            metric_logger.update(loss=loss.item(), lr=training_box.optimizer.param_groups[0]['lr'])
+        metric_logger.meters['img/s'].update(batch_size / (time.time() - start_time))
+        if (torch.isnan(loss) or torch.isinf(loss)) and is_main_process():
+            raise ValueError('The training loop was broken due to loss = {}'.format(loss))
+
+def train(teacher_model, student_model, dataset_dict, ckpt_file_path, device, device_ids, distributed, config, args, writer):
+    logger.info('Start training')
+    train_config = config['train']
+    lr_factor = args.world_size if distributed and args.adjust_lr else 1
+    training_box = get_training_box(student_model, dataset_dict, train_config,
+                                    device, device_ids, distributed, lr_factor) if teacher_model is None \
+        else get_distillation_box(teacher_model, student_model, dataset_dict, train_config,
+                                  device, device_ids, distributed, lr_factor)
+    best_val_top1_accuracy = 0.0
+    optimizer, lr_scheduler = training_box.optimizer, training_box.lr_scheduler
+    if file_util.check_if_exists(ckpt_file_path):
+        best_val_top1_accuracy, _, _ = load_ckpt(ckpt_file_path, optimizer=optimizer, lr_scheduler=lr_scheduler)
+
+    log_freq = train_config['log_freq']
+    student_model_without_ddp = student_model.module if module_util.check_if_wrapped(student_model) else student_model
+    aux_module = student_model_without_ddp.get_aux_module() if check_if_updatable(student_model_without_ddp) else None
+    epoch_to_update = train_config.get('epoch_to_update', None)
+    bottleneck_updated = False
+    no_dp_eval = args.no_dp_eval
+    start_time = time.time()
+    for epoch in range(args.start_epoch, training_box.num_epochs):
+        training_box.pre_process(epoch=epoch)
+        if epoch_to_update is not None and epoch_to_update <= epoch and not bottleneck_updated:
+            logger.info('Updating entropy bottleneck')
+            student_model_without_ddp.update()
+            bottleneck_updated = True
+
+        train_one_epoch(training_box, aux_module, bottleneck_updated, device, epoch, log_freq)
+        val_top1_accuracy = evaluate(student_model, training_box.val_data_loader, device, device_ids, distributed,
+                                     no_dp_eval=no_dp_eval, log_freq=log_freq, header='Validation:', writer = writer)
+        writer.add_scalar('validation top1 accuracy', val_top1_accuracy, epoch+1)
+        if val_top1_accuracy > best_val_top1_accuracy and is_main_process():
+            logger.info('Best top-1 accuracy: {:.4f} -> {:.4f}'.format(best_val_top1_accuracy, val_top1_accuracy))
+            logger.info('Updating ckpt at {}'.format(ckpt_file_path))
+            best_val_top1_accuracy = val_top1_accuracy
+            save_ckpt(student_model_without_ddp, optimizer, lr_scheduler,
+                      best_val_top1_accuracy, config, args, ckpt_file_path)
+        training_box.post_process()
+
+    if epoch == 20:
+        for i in [1,3,6,8,12,14,16,19]:
+            exec(f'student_model.backbone.features.body.bottleneck_layer.max{i}.clear()')
+            evaluate(student_model, training_box.val_data_loader, device, device_ids, distributed,
+                    no_dp_eval=no_dp_eval, log_freq=log_freq, header='Validation:', writer=writer)
+            exec(f'logger.info(max(student_model.backbone.features.body.bottleneck_layer.max{i}))')
+
+    if distributed:
+        dist.barrier()
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    logger.info('Training time {}'.format(total_time_str))
+    training_box.clean_modules()
+
+
+def main(args):
+    log_file_path = args.log
+    if is_main_process() and log_file_path is not None:
+        setup_log_file(os.path.expanduser(log_file_path))
+
+    # distributed, device_ids = init_distributed_mode(args.world_size, args.dist_url)
+    distributed, device_ids = False, None
+    logger.info(args)
+    cudnn.benchmark = True
+    cudnn.deterministic = True
+    set_seed(args.seed)
+    config = yaml_util.load_yaml_file(os.path.expanduser(args.config))
+    if args.json is not None:
+        logger.info('Overwriting config')
+        overwrite_config(config, json.loads(args.json))
+
+    device = torch.device(args.device)
+    dataset_dict = util.get_all_datasets(config['datasets'])
+
+    train_batch_size=config['train']['train_data_loader']['batch_size']
+    train_shuffle=config['train']['train_data_loader']['random_sample']
+    train_num_workers=config['train']['train_data_loader']['num_workers']
+    train_data_loader_config = config['train']['train_data_loader']
+    train_data_loader = util.build_data_loader(dataset_dict[train_data_loader_config['dataset_id']],
+                                                train_data_loader_config, distributed)
+    # print(int(0.2*len(train_data_loader))/100)
+    # 25 imgs per class
+    subsampler = DatasetSampling(train_data_loader.dataset,25)
+    index_dataset=subsampler.listindex()
+    data_subset=Subset(train_data_loader.dataset, index_dataset)
+    subset_dataloader = DataLoader(data_subset,batch_size=train_batch_size, shuffle=train_shuffle,pin_memory=True,num_workers=train_num_workers)
+
+    models_config = config['models']
+    student_model_config =\
+        models_config['student_model'] if 'student_model' in models_config else models_config['model']
+    
+    ckpt_file_path = student_model_config.get('ckpt', None)
+    student_model = load_model(student_model_config, device, distributed)
+    ranger_student = fix_model(student_model, [20,10])
+    # logger.info(student_model)
+    if args.log_config:
+        logger.info(config)
+
+    if not args.test_only:
+        writer = SummaryWriter(os.path.join(args.writerlog, 'mobilenet'))
+        train(teacher_model, student_model, dataset_dict, ckpt_file_path, device, device_ids, distributed, config, args, writer)
+        student_model_without_ddp =\
+            student_model.module if module_util.check_if_wrapped(student_model) else student_model
+        load_ckpt(student_model_config['ckpt'], model=student_model_without_ddp, strict=True)
+
+    test_config = config['test']
+    test_data_loader_config = test_config['test_data_loader']
+    test_data_loader = util.build_data_loader(dataset_dict[test_data_loader_config['dataset_id']],
+                                              test_data_loader_config, distributed)
+    log_freq = test_config.get('log_freq', 1000)
+    no_dp_eval = args.no_dp_eval
+    if not args.student_only and teacher_model is not None:
+        evaluate(teacher_model, test_data_loader, device, device_ids, distributed, no_dp_eval=no_dp_eval,
+                 log_freq=log_freq, title='[Teacher: {}]'.format(teacher_model_config['name']))
+
+    if check_if_updatable(student_model):
+        student_model.update()
+
+    if check_if_analyzable(student_model):
+        student_model.activate_analysis()
+    evaluate(student_model, test_data_loader, device, device_ids, distributed, no_dp_eval=no_dp_eval,
+             log_freq=log_freq, title='[Student: {}]'.format(student_model_config['name']))
+
+
+if __name__ == '__main__':
+    argparser = get_argparser()
+    main(argparser.parse_args())
